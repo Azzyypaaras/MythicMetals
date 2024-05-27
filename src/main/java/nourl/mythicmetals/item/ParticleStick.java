79bcd7392e206fb48b6cdf982c992e3be0263c5e
Noaaan <noaaan@hotmail.com>
2024-05-27 17:02:50 +0200

    Make ParticleSticks generic
    Supports the extra data field
    
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/nourl/mythicmetals/item/ParticleStick.java
+++ src/main/java/nourl/mythicmetals/item/ParticleStick.java
@@ -8,18 +8,30 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
-public class ParticleStick extends Item {
-    private final ParticleSystem<?> particle;
+public class ParticleStick<T> extends Item {
+    private final ParticleSystem<T> particle;
+    private final T extraData;
 
-    public ParticleStick(Settings settings, ParticleSystem<?> particle) {
+    public ParticleStick(Settings settings, ParticleSystem<T> particle) {
         super(settings);
         this.particle = particle;
+        this.extraData = null;
+    }
+
+    public ParticleStick(Settings settings, ParticleSystem<T> particle, T data) {
+        super(settings);
+        this.particle = particle;
+        this.extraData = data;
     }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         var stack = user.getStackInHand(hand);
-        particle.spawn(world, user.getPos());
+        if (this.extraData != null) {
+            particle.spawn(world, user.getPos(), extraData);
+        } else {
+            particle.spawn(world, user.getPos());
+        }
         return TypedActionResult.pass(stack);
     }
 
