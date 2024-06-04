33840976eb00ed6e21d3ade24b8911b88e58fbce
Noaaan <noaaan@hotmail.com>
2024-06-04 20:24:55 +0200

    Add a tooltip handler to the PrometheumComponent
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/nourl/mythicmetals/component/PrometheumComponent.java
+++ src/main/java/nourl/mythicmetals/component/PrometheumComponent.java
@@ -2,20 +2,23 @@ package nourl.mythicmetals.component;
 
 import io.wispforest.owo.serialization.StructEndec;
 import io.wispforest.owo.serialization.endec.StructEndecBuilder;
+import net.minecraft.client.item.TooltipType;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import nourl.mythicmetals.data.MythicTags;
 import java.util.UUID;
+import java.util.function.Consumer;
 
-public record PrometheumComponent(int durabilityRepaired) {
+public record PrometheumComponent(int durabilityRepaired) implements TooltipAppender {
     public static final int OVERGROWN_THRESHOLD = 1200;
     public static final StructEndec<PrometheumComponent> ENDEC = StructEndecBuilder.of(
-      StructEndec.INT.fieldOf("durability_repaired", PrometheumComponent::durabilityRepaired),
-      PrometheumComponent::new
+        StructEndec.INT.fieldOf("durability_repaired", PrometheumComponent::durabilityRepaired),
+        PrometheumComponent::new
     );
     public static final UUID ARMOR_BONUS_ID = UUID.fromString("30d14ea3-8513-4914-8e5b-54084d6a450b");
     public static final UUID TOUGHNESS_BONUS_ID = UUID.fromString("6d75e1c4-eec6-4a6a-aeec-815d7fa06c90");
@@ -94,5 +97,18 @@ public record PrometheumComponent(int durabilityRepaired) {
             "Overgrown Prometheum bonus",
             bonus,
             EntityAttributeModifier.Operation.ADD_VALUE);
+    }
+
+    // TODO - Use TooltipCallback to render this tooltip on tagged entries
+    @Override
+    public void appendTooltip(Item.TooltipContext context, Consumer<Text> tooltip, TooltipType type) {
+        tooltip.accept(Text.translatable("abilities.mythicmetals.mending"));
+        if (this.isOvergrown()) {
+            tooltip.accept(Text.translatable("tooltip.prometheum.overgrown"));
+        }
+        // FIXME - Move engrained to a TooltipCallback?
+        //if (EnchantmentHelper.hasBindingCurse(stack)) {
+        //    Ability.addTooltipOnStack(stack, lines, Style.EMPTY.withColor(UsefulSingletonForColorUtil.MetalColors.PROMETHEUM.rgb()), "tooltip.prometheum.engrained");
+        //}
     }
 }
